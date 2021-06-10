67b4314d7e16c2b474f74c442f74964a62459896
AKNDO <mzjbvqcsdbvl@mail.ru>
2021-06-10 16:28:14 +0300

    Create Accounting Service

---
+++ HomeAccounting.BusinessLogic/AcountingService.cs
@@ -0,0 +1,30 @@
+﻿using HomeAccounting.BusinessLogic.Contract;
+using HomeAccounting.DataSource.Contract;
+
+namespace HomeAccounting.BusinessLogic
+{
+    public class AcountingService : IAccounting
+    {
+        private readonly IRepository _repository;
+
+        public AcountingService(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public void CreateAccount(Account account)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Account GetAccountById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SaveAccount(Account account)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
